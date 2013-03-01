10c81606d99acffe0b43b8c2a27bb1cd0d3fbafa
abnormal <abnormal@localhost.localdomain>
2013-03-01 14:06:36 +0100

    Different implementation for ip address defining

--- src/java/no/hin/stg/servlet/AppStartListener.java
+++ src/java/no/hin/stg/servlet/AppStartListener.java
@@ -1,8 +1,9 @@
-
 package no.hin.stg.servlet;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,29 +23,43 @@ public class AppStartListener implements ServletContextListener {
 
     /**
      * <p>Application server will execute this mthod automatically after
-     * application deploy. 
+     * application deploy.
      *
      * @param sce ServletContext event
      */
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         try {//create and register multcast DNS service
-            jmdns = JmDNS.create(InetAddress.getLocalHost(), "service");
-            HashMap<String, String> params = new HashMap<String, String>(5);
-            params.put("ServiceURL", sce.getServletContext().getContextPath() + "/Oblig3Service");
-            params.put("WSDLURL", sce.getServletContext().getContextPath() + "/Oblig3Service?WSDL");
-            ServiceInfo info = ServiceInfo.create("_http._tcp.local", "WebService", 8080, 0, 0, params);
-            jmdns.registerService(info);
-            sce.getServletContext().setAttribute("message", "JmDNS service is started on " + InetAddress.getLocalHost() + " succesfully");
+            Enumeration<NetworkInterface> cards = NetworkInterface.getNetworkInterfaces();
+            while (cards.hasMoreElements()) {
+                NetworkInterface current = cards.nextElement();
+                if (current.isLoopback() || current.isVirtual() || current.isPointToPoint() || !current.isUp() || !current.supportsMulticast()) {
+                    break;
+                }
+                Enumeration<InetAddress> ipes = current.getInetAddresses();
+                while (ipes.hasMoreElements()) {
+                    InetAddress currentIp = ipes.nextElement();
+                    jmdns = JmDNS.create(InetAddress.getByAddress(currentIp.getAddress()));
+                    HashMap<String, String> params = new HashMap<String, String>(5);
+                    params.put("ServiceURL", sce.getServletContext().getContextPath() + "/Oblig3Service");
+                    params.put("WSDLURL", sce.getServletContext().getContextPath() + "/Oblig3Service?WSDL");
+                    ServiceInfo info = ServiceInfo.create("_http._tcp.local", "WebService", 8080, 0, 0, params);
+                    jmdns.registerService(info);
+                    sce.getServletContext().setAttribute("message", "JmDNS service is started on " + currentIp.getHostAddress() + " succesfully");
+                }
+
+            }
+
         } catch (IOException ex) {
             Logger.getLogger(AppStartListener.class.getName()).log(Level.SEVERE, null, ex);
             sce.getServletContext().setAttribute("message", "JmDNS service is not started due " + ex.getMessage());
         }
     }
 
-
     /**
-     *<p>Application server will execute this mthod automatically before application undeploy
+     * <p>Application server will execute this mthod automatically before
+     * application undeploy
+     *
      * @param sce
      */
     @Override
